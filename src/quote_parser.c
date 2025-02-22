008689246a1575faf8a5c3a537d08c3b4c59ca6b
Galbio <101997612+Galbio@users.noreply.github.com>
2025-02-22 23:40:15 +0100

    quote parsing function (#4)
    * quote parser len done (i think)
    
    * add include file
    
    * quote parser really done (i still think)
    
    * fixed slight oversight
    
    * fixed slight oversight... again
    
    ---------
    
    Co-authored-by: Gabriel Karbouch <gakarbou@c3r4p9.42perpignan.fr>

---
+++ src/quote_parser.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quote_parser.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gakarbou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 21:21:54 by gakarbou          #+#    #+#             */
+/*   Updated: 2025/02/22 23:38:20 by gakarbou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	check_special_char(char c, char *backslash, char *cur_quote)
+{
+	if ((c == '\'') && (*cur_quote == '\''))
+		return (*cur_quote = 0, 0);
+	else if ((*cur_quote == 0) && (c == '\''))
+		return (*cur_quote = '\'', 0);
+	else if (*cur_quote == '\'')
+		return (1);
+	else if ((c == '"') && (*cur_quote == '"') && (*backslash))
+		return (*backslash = 0, 1);
+	else if ((c == '"') && (*cur_quote == '"'))
+		return (*cur_quote = 0, 0);
+	else if ((*cur_quote == 0) && (c == '"'))
+		return (*cur_quote = '"', 0);
+	else if (c == '"')
+		return (*cur_quote = '"', 0);
+	if ((c == '\\') && (*backslash))
+		return (*backslash = 0, 1);
+	else if (c == '\\')
+		return (*backslash = 1, 0);
+	else
+		*backslash = 0;
+	return (1);
+}
+
+int	get_parsed_len(char *str)
+{
+	char	back_slashed;
+	char	cur_quote;
+	int		res;
+	int		i;
+
+	i = -1;
+	res = 0;
+	cur_quote = 0;
+	back_slashed = 0;
+	while (str[++i])
+	{
+		if (!check_special_char(str[i], &back_slashed, &cur_quote))
+			continue ;
+		res++;
+	}
+	return (res);
+}
+
+char	*parse_quotes(char *str, int i)
+{
+	char	back_slashed;
+	char	*dest;
+	char	cur_quote;
+	int		res;
+
+	dest = malloc(sizeof(char) * (get_parsed_len(str)));
+	if (!dest)
+		return (NULL);
+	res = 0;
+	cur_quote = 0;
+	back_slashed = 0;
+	while (str[++i])
+	{
+		if (!check_special_char(str[i], &back_slashed, &cur_quote))
+			continue ;
+		dest[res++] = str[i];
+	}
+	dest[res] = 0;
+	return (dest);
+}
