5f85ac7813c452d0795b00b27250bfa39c60eb83
Gabriel Karbouch <gakarbou@c3r4p9.42perpignan.fr>
2025-02-22 23:29:49 +0100

    quote parser really done (i still think)

--- src/quote_parser.c
+++ src/quote_parser.c
@@ -6,39 +6,29 @@
 /*   By: gakarbou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 21:21:54 by gakarbou          #+#    #+#             */
-/*   Updated: 2025/02/22 22:38:10 by gakarbou         ###   ########.fr       */
+/*   Updated: 2025/02/22 23:29:33 by gakarbou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
 char	check_special_char(char c, char *backslash, char *cur_quote)
 {
-	if ((c == '"') && (*cur_quote == '\''))
-		 return (1);
-	else if ((c == '"') && (*cur_quote == '"') && (*backslash))
-		return (0);
-	else if ((c == '"') && (*cur_quote == '"'))
-		 *cur_quote = 0;
-	else if ((c == '\'') && (*cur_quote == '\''))
-		*cur_quote = 0;
-	else if ((*cur_quote == 0) && (c == '"'))
-		*cur_quote = '"';
+	if ((c == '\'') && (*cur_quote == '\''))
+		return (*cur_quote = 0, 0);
 	else if ((*cur_quote == 0) && (c == '\''))
-		*cur_quote = '\'';
+		return (*cur_quote = '\'', 0);
+	else if (*cur_quote == '\'')
+		return (1);
+	else if ((c == '"') && (*cur_quote == '"') && (*backslash))
+		return (*backslash = 0, 1);
+	else if ((c == '"') && (*cur_quote == '"'))
+		return (*cur_quote = 0, 0);
+	else if ((*cur_quote == 0) && (c == '"'))
+		return (*cur_quote = '"', 0);
 	else if (c == '"')
-		*cur_quote = '"';
-	else
+		return (*cur_quote = '"', 0);
+	else if (*cur_quote == '\'')
 		return (1);
 	if ((c == '\\') && (*backslash))
 		return (*backslash = 0, 1);
@@ -46,19 +36,18 @@ char	check_special_char(char c, char *backslash, char *cur_quote)
 		return (*backslash = 1, 0);
 	else
 		*backslash = 0;
-	return (0);
+	return (1);
 }
 
-int		get_parsed_len(char *str, int len)
+int	get_parsed_len(char *str, int i)
 {
 	char	back_slashed;
 	char	cur_quote;
 	int		res;
-	int		i;
 
-	i = -1;
 	res = 0;
 	cur_quote = 0;
+	back_slashed = 0;
 	while (str[++i])
 	{
 		if (!check_special_char(str[i], &back_slashed, &cur_quote))
@@ -68,21 +57,25 @@ int		get_parsed_len(char *str, int len)
 	return (res);
 }
 
-char	*parse_quotes(char *str)
+char	*parse_quotes(char *str, int i)
 {
+	char	back_slashed;
 	char	*dest;
+	char	cur_quote;
+	int		res;
 
-	printf("%d\n", get_parsed_len(str, ft_strlen(str)));
-	exit(0);
-	dest = malloc(sizeof(char) * (get_parsed_len(str, ft_strlen(str)) + 1));
-}
-
-int	main(int argc, char **argv)
-{
-	char	*nice;
-
-	nice = readline("");
-	printf("Og = %s\n", nice);
-	parse_quotes(nice);
-	return (0);
+	dest = malloc(sizeof(char) * (get_parsed_len(str, -1)));
+	if (!dest)
+		return (NULL);
+	res = 0;
+	cur_quote = 0;
+	back_slashed = 0;
+	while (str[++i])
+	{
+		if (!check_special_char(str[i], &back_slashed, &cur_quote))
+			continue ;
+		dest[res++] = str[i];
+	}
+	dest[res] = 0;
+	return (dest);
 }
