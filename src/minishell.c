2cac5367ef103e2ec8dfea434eb6baffa36a23b7
lroussel <lroussel@student.42perpignan.fr>
2025-02-23 20:37:45 +0100

    fix conflict files

--- src/minishell.c
+++ src/minishell.c
@@ -6,19 +6,12 @@
 /*   By: lroussel <lroussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 22:50:10 by lroussel          #+#    #+#             */
-<<<<<<< Updated upstream
-/*   Updated: 2025/02/23 13:43:52 by gakarbou         ###   ########.fr       */
-=======
 /*   Updated: 2025/02/23 13:50:35 by lroussel         ###   ########.fr       */
->>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-<<<<<<< Updated upstream
-void	launch(t_list *envp, t_main_envp *imp)
-=======
 void	init(void)
 {
 	struct termios	raw;
@@ -26,8 +19,7 @@ void	init(void)
 	enable_raw_mode(&raw);
 }
 
-void	launch(void)
->>>>>>> Stashed changes
+void	launch(t_list *envp, t_main_envp *imp)
 {
 	char	*res;
 	char	*str;
