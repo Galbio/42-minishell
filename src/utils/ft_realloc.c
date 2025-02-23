9743e8bf478ac0820f7e711e2ee23316a2f586f8
lroussel <lroussel@student.42perpignan.fr>
2025-02-23 01:49:44 +0100

    add ft_readline (simply version)

--- src/utils/ft_realloc.c
+++ src/utils/ft_realloc.c
@@ -1,25 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lroussel <lroussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/22 22:50:10 by lroussel          #+#    #+#             */
-/*   Updated: 2025/02/23 01:35:06 by lroussel         ###   ########.fr       */
+/*   Created: 2025/02/23 00:50:21 by lroussel          #+#    #+#             */
+/*   Updated: 2025/02/23 00:51:17 by lroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	launch(void)
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
-	char	*res;
+	void	*temp;
 
-	while (1)
+	if (!new_size)
+		new_size = 1;
+	temp = malloc(new_size);
+	if (!temp)
+		return (NULL);
+	if (ptr)
 	{
-		res = ft_readline("ecris stp > ");
-		free(res);
-
+		if (old_size < new_size)
+			new_size = old_size;
+		ft_memcpy(temp, ptr, new_size);
+		free(ptr);
 	}
+	return (temp);
 }
