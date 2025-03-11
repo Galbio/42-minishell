52152e4888357a635085dd5387c981a8e2840d8c
Gabriel Karbouch <gakarbou@c2r5p3.42perpignan.fr>
2025-03-11 15:35:30 +0100

    done

--- src/commands/execute_bin.c
+++ src/commands/execute_bin.c
@@ -6,11 +6,7 @@
 /*   By: gakarbou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 20:09:48 by gakarbou          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/03/05 16:59:09 by gakarbou         ###   ########.fr       */
-=======
-/*   Updated: 2025/03/05 17:24:54 by gakarbou         ###   ########.fr       */
->>>>>>> refs/heads/main
+/*   Updated: 2025/03/11 15:35:19 by gakarbou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,14 +81,8 @@ char	*execute_bin(char **argv, t_main_envp *imp)
 	path = get_command_path(argv[0], imp->path);
 	if (!path)
 	{
-<<<<<<< HEAD
 		ft_putstr_fd(argv[0], 2);
 		ft_putstr_fd(": command not found\n", 2);
-=======
-		if (!imp->path)
-			ft_putstr_fd("minishell: ", 2);
-		printf("%s: command not found\n", argv[0]);
->>>>>>> refs/heads/main
 		return (NULL);
 	}
 	temp = argv[0];
