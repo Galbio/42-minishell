6b8e8e11498d083613e317e4b35efe82bb0869cd
Gabriel Karbouch <gakarbou@c2r5p1.42perpignan.fr>
2025-03-18 02:05:51 +0100

    fix small bug

--- src/argv/var_parser.c
+++ src/argv/var_parser.c
@@ -6,7 +6,7 @@
 /*   By: gakarbou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 03:46:01 by gakarbou          #+#    #+#             */
-/*   Updated: 2025/03/18 01:54:04 by gakarbou         ###   ########.fr       */
+/*   Updated: 2025/03/18 02:04:44 by gakarbou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,6 +99,6 @@ void	handle_var(char *str, t_int_tab *itab, t_list **cmd_outputs,
 		output = get_var_value(itab->ptr1, *(cmd.envp));
 	ft_lstadd_back(cmd_outputs, ft_lstnew(output));
 	itab->res += ft_strlen(output);
-	itab->i += ft_strlen(itab->ptr1) + 1;
+	itab->i += ft_strlen(itab->ptr1) + (itab->ptr1[0] == '(');
 	free(itab->ptr1);
 }
