c4b52d188448010a16e6013be93a4a32c4369d46
Gabriel Karbouch <gakarbou@c2r5p3.42perpignan.fr>
2025-03-20 20:27:44 +0100

    everything works wtf

--- src/utils/check_special_char.c
+++ src/utils/check_special_char.c
@@ -6,7 +6,7 @@
 /*   By: gakarbou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 20:51:10 by gakarbou          #+#    #+#             */
-/*   Updated: 2025/03/20 18:22:39 by gakarbou         ###   ########.fr       */
+/*   Updated: 2025/03/20 20:24:43 by gakarbou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,14 @@ char	check_special_char(char *str, t_int_tab *itab)
 		return (itab->cur_quote = '\'', 1);
 	if (itab->cur_quote == '\'')
 		return (0);
-	if (!itab->backslash && (str[itab->i] == '\\') && ft_strchr("$\\\"", str[itab->i + 1]))
+	if (!itab->backslash && (str[itab->i] == '\\') && !itab->cur_quote
+		&& ft_strchr(" \n\t\\", str[itab->i]))
+		return (itab->backslash = 1, 0);
+	if (!itab->backslash && (str[itab->i] == '\\')
+		&& ft_strchr("$\\\"", str[itab->i + 1]))
 		return (itab->backslash = 1, 1);
 	if (itab->backslash && !itab->cur_quote && ft_strchr(" \n\t", str[itab->i])
 		&& (itab->i && str[itab->i - 1] == '\\'))
 		return (0);
-	if (!itab->backslash && (str[itab->i] == '\\') && !itab->cur_quote
-		&& ft_iswhitespace(str[itab->i + 1]))
-		return (itab->backslash = 1, 0);
 	return (check_other_special_char(str, itab));
 }
