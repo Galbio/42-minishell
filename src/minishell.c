df28d564dce2c6b64c139d189db329734e576e1b
lroussel <lroussel@student.42perpignan.fr>
2025-03-25 19:17:21 +0100

    remove tests

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: lroussel <lroussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 22:50:10 by lroussel          #+#    #+#             */
-/*   Updated: 2025/03/25 18:24:00 by lroussel         ###   ########.fr       */
+/*   Updated: 2025/03/25 17:45:54 by lroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ void	launch(t_list *envp, t_main_envp *imp)
 			free(res);
 			continue ;
 		}
-		printf("RES = |%s|\n", res);
 		if (ft_strchr(res, '`'))
 			res = handle_bquotes(res);
 		imp->output_fd = 1;
