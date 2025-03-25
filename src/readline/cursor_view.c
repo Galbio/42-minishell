f291261a156dcc907ef29d5264685c4eb4e45bc6
lroussel <lroussel@student.42perpignan.fr>
2025-03-25 15:48:54 +0100

    review changes

--- src/readline/cursor_view.c
+++ src/readline/cursor_view.c
@@ -6,7 +6,7 @@
 /*   By: lroussel <lroussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 13:46:41 by lroussel          #+#    #+#             */
-/*   Updated: 2025/03/25 14:37:33 by lroussel         ###   ########.fr       */
+/*   Updated: 2025/03/25 15:47:16 by lroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	hide_cursor(void)
 {
-	write(0, HIDE_CURSOR, ft_strlen(HIDE_CURSOR));
+	write(0, HIDE_CURSOR, 6);
 }
 
 void	show_cursor(void)
 {
-	write(0, SHOW_CURSOR, ft_strlen(SHOW_CURSOR));
+	write(0, SHOW_CURSOR, 6);
 }
