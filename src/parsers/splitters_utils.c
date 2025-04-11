5819789b248ad81e44f1fc2fc0566670d5fcdaf0
root <root@PC.>
2025-04-11 02:30:23 +0200

    yet another fix

--- src/parsers/splitters_utils.c
+++ src/parsers/splitters_utils.c
@@ -6,7 +6,7 @@
 /*   By: gakarbou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 22:54:49 by gakarbou          #+#    #+#             */
-/*   Updated: 2025/04/11 02:04:31 by gakarbou         ###   ########.fr       */
+/*   Updated: 2025/04/11 02:29:37 by gakarbou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,8 @@ int	add_cmd(char *str, t_list **dest, t_int_tab *itab)
 	char	*temp;
 
 	temp = ft_substr(str, itab->ret, itab->i - itab->ret);
-	if (ft_isonlywhitespaces(temp) || ft_isonlywhitespaces(str + itab->i))
+	if (!ft_strncmp(str + itab->i, ";;", 2) || ft_isonlywhitespaces(temp)
+		|| ft_isonlywhitespaces(str + itab->i))
 	{
 		free(temp);
 		print_error(str, itab);
@@ -53,14 +54,15 @@ int	add_cmd(char *str, t_list **dest, t_int_tab *itab)
 	}
 	ft_lstadd_back(dest, ft_lstnew(trim_ws(temp)));
 	itab->ret = (itab->i + 1 + (str[itab->i] != ';'));
-	itab->i += (str[itab->i] != ';');
 	if (!str[itab->ret - 1])
 		itab->ret--;
 	if (!str[itab->ret] || ft_strchr("|&", str[itab->ret]))
 	{
-		itab->i++;
+		if (str[itab->ret])
+			itab->i += 2;
 		print_error(str, itab);
 		return (1);
 	}
+	itab->i += (str[itab->i] != ';');
 	return (0);
 }
