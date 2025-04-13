e93a73d69337afb0d8f67b499ab7885f81416e5e
Galbio <101997612+Galbio@users.noreply.github.com>
2025-04-13 18:43:09 +0200

    Fix variable bugs (#143)
    * yeah
    
    * header
    
    * leak
    
    ---------
    
    Co-authored-by: Gabriel Karbouch <gakarbou@c2r5p3.42perpignan.fr>

--- src/argv/argv_utils.c
+++ src/argv/argv_utils.c
@@ -6,34 +6,13 @@
 /*   By: gakarbou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 20:57:20 by gakarbou          #+#    #+#             */
-/*   Updated: 2025/04/08 22:25:19 by gakarbou         ###   ########.fr       */
+/*   Updated: 2025/04/13 18:01:29 by gakarbou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	add_var_to_dest(char *dest, char *str, t_int_tab *itab,
-		t_list **cmd_outputs)
-{
-	char		*to_add;
-	int			len;
-	t_list		*temp;
-
-	to_add = ((char *)(*cmd_outputs)->content);
-	len = ft_strlen(to_add);
-	ft_memcpy(dest + itab->res, to_add, len);
-	temp = *cmd_outputs;
-	*cmd_outputs = temp->next;
-	free(temp->content);
-	free(temp);
-	itab->res += len;
-	if (ft_strchr("{(", str[itab->i + 1]))
-		itab->i += get_subcmd_size(str + itab->i + 1);
-	else if (!ft_strchr("~%+=]}./\\:\0", str[itab->i + 1]))
-		itab->i += go_to_var_end(str + itab->i) - 1;
-}
-
-static int	get_parsed_size(char *str, t_list **cmd_outputs, t_cmd_params *cmd)
+static int	get_parsed_size(char *str)
 {
 	t_int_tab	itab;
 
@@ -43,8 +22,6 @@ static int	get_parsed_size(char *str, t_list **cmd_outputs, t_cmd_params *cmd)
 		itab.backslash = itab.i && (str[itab.i - 1] == '\\') && !itab.backslash;
 		if (check_special_char(str, &itab))
 			continue ;
-		if ((str[itab.i] == '$') && (itab.cur_quote != '\'') && !itab.backslash)
-			handle_var(str, &itab, cmd_outputs, cmd);
 		else
 		{
 			if (itab.cur_quote && ft_strchr(" \n\t\\", str[itab.i]))
@@ -61,42 +38,35 @@ static int	get_parsed_size(char *str, t_list **cmd_outputs, t_cmd_params *cmd)
 	return (itab.res);
 }
 
-static void	fill_dest(char *dest, t_int_tab *itab, char *str,
-			t_list **cmd_outputs)
+static void	fill_dest(char *dest, t_int_tab *itab, char *str)
 {
 	itab->backslash = itab->i && (str[itab->i - 1] == '\\') && !itab->backslash;
 	if (check_special_char(str, itab))
 		return ;
-	if ((str[itab->i] == '$') && (itab->cur_quote != '\'') && !itab->backslash)
-		add_var_to_dest(dest, str, itab, cmd_outputs);
-	else
-	{
-		if (itab->cur_quote && ft_strchr(" \n\t\\", str[itab->i]))
-			dest[itab->res++] = '\\';
-		else if (ft_strchr("*[?", str[itab->i])
-			&& (itab->cur_quote || itab->backslash))
-			dest[itab->res++] = '\\';
-		dest[itab->res++] = str[itab->i];
-		if (!itab->cur_quote && itab->backslash
-			&& (str[itab->i] == '\\') && (str[itab->i + 1] == '\\'))
-			return ;
-	}
+	if (itab->cur_quote && ft_strchr(" \n\t\\", str[itab->i]))
+		dest[itab->res++] = '\\';
+	else if (ft_strchr("*[?", str[itab->i])
+		&& (itab->cur_quote || itab->backslash))
+		dest[itab->res++] = '\\';
+	dest[itab->res++] = str[itab->i];
+	if (!itab->cur_quote && itab->backslash
+		&& (str[itab->i] == '\\') && (str[itab->i + 1] == '\\'))
+		return ;
 }
 
 char	*parse_quotes(char *str, t_cmd_params *cmd)
 {
 	char		*dest;
-	t_list		*cmd_outputs;
 	t_int_tab	itab;
 
 	itab = init_int_tab();
-	cmd_outputs = NULL;
-	itab.ret = get_parsed_size(str, &cmd_outputs, cmd);
+	str = replace_var(str, cmd);
+	itab.ret = get_parsed_size(str);
 	dest = malloc(sizeof(char) * (itab.ret + 1));
 	if (!dest)
 		return (NULL);
 	while (str[++itab.i])
-		fill_dest(dest, &itab, str, &cmd_outputs);
+		fill_dest(dest, &itab, str);
 	dest[itab.ret] = 0;
 	free(str);
 	return (dest);
--- src/argv/parse_cmd_return.c
+++ src/argv/parse_cmd_return.c
@@ -6,13 +6,13 @@
 /*   By: gakarbou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 15:53:40 by gakarbou          #+#    #+#             */
-/*   Updated: 2025/04/08 22:31:45 by gakarbou         ###   ########.fr       */
+/*   Updated: 2025/04/13 18:18:09 by gakarbou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	*remove_end_newlines(char *str)
+char	*parse_var_return(char *str)
 {
 	int		len;
 	char	*dest;
@@ -25,50 +25,8 @@ static char	*remove_end_newlines(char *str)
 	dest = malloc(sizeof(char) * (len + 1));
 	if (!dest)
 		return (NULL);
+	ft_memcpy(dest, str, len);
 	dest[len] = 0;
-	while (--len >= 0)
-		dest[len] = str[len];
-	free(str);
-	return (dest);
-}
-
-static int	get_return_size(char *str, char quote)
-{
-	int		ret;
-	int		i;
-
-	ret = 0;
-	i = -1;
-	while (str[++i])
-	{
-		if ((str[i] == '\\') || (quote && ft_strchr(" \n\t", str[i]))
-			|| ft_strchr(" \n\t", str[i]))
-			ret++;
-	}
-	return (ret + i);
-}
-
-char	*parse_var_return(char *str, char quote)
-{
-	char	*dest;
-	int		ret;
-	int		i;
-
-	str = remove_end_newlines(str);
-	ret = get_return_size(str, quote);
-	dest = malloc(sizeof(char) * (ret + 1));
-	if (!dest)
-		return (NULL);
-	ret = 0;
-	i = -1;
-	while (str[++i])
-	{
-		if ((str[i] == '\\') || (quote && ft_strchr(" \t\n", str[i]))
-			|| ft_strchr("*[?", str[i]))
-			dest[ret++] = '\\';
-		dest[ret++] = str[i];
-	}
-	dest[ret] = 0;
 	free(str);
 	return (dest);
 }
