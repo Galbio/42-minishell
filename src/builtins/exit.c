1e1016f45299763d0de3db79193c1469be547e33
lroussel <lroussel@student.42perpignan.fr>
2025-04-13 19:37:32 +0200

    fix

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: gakarbou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 15:05:06 by gakarbou          #+#    #+#             */
-/*   Updated: 2025/04/13 02:35:22 by gakarbou         ###   ########.fr       */
+/*   Updated: 2025/04/13 19:36:30 by lroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ int	ms_exit(t_cmd_params *cmd)
 	res = get_exit_status();
 	av1 = cmd->argv[1] != NULL;
 	if (!get_depth(0))
-		write(2, "exit\n", 5);
+		write(STDOUT_FILENO, "exit\n", 5);
 	if (!av1 || (parse_exit(cmd) == 1))
 	{
 		free_cmd(cmd, 1);
